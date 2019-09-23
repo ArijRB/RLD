e4a3fd6169ecb8846e9049749cf1b1b54f16a71f
RootNico <nicopsp@live.fr>
2019-09-23 21:39:29 +0200

    Policy itération finit

--- TME2-RDL/RL/policy_Agent.py
+++ TME2-RDL/RL/policy_Agent.py
@@ -11,37 +11,43 @@ import copy
 class PolicyAgent(object):
     """The world's simplest agent!"""
 
-    def __init__(self, action_space):
+    def __init__(self, action_space,states):
         self.action_space = action_space
         self.V = None
         self.Pi = None
-        self.gamma = 0.99
+        self.gamma = 0.999
+        self.states = states
 
     def act(self, observation, reward, done):
-        return self.Pi[observation]
-    
-    def fit(self,mdp):
-        self.V = np.random.random(len(mdp))
-        self.Pi = np.random.randint(0,self.action_space.n,len(mdp))
-        mdp = list(mdp.values())
-        nb_iter = 100
+        return self.Pi[self.states[gridworld.GridworldEnv.state2str(observation)]]
+
+    def fit(self,statedic, mdp):
+        self.V = np.random.random(len(statedic))
+        self.Pi = np.random.randint(0,self.action_space.n,len(statedic))
+        nb_iter = 100000
+        epsilon = 0.001
         for _ in range(nb_iter):
-            for i in range(len(self.V)):
+            ancien_V = self.V
+            for cle_s,valeur_s in statedic.items():
+                if(cle_s not in mdp.keys()):continue
                 somme = 0
-                for j in range(len(mdp)):
-                    somme += mdp[i][self.Pi[state[i]]][j][0]*( mdp[i][self.Pi[i]][j][2] + self.gamma*self.V[i])
-                self.V[i] = somme
-            for i in range(len(self.V)):
-                li = np.zeros(len(self.action_space))
-                for a in range(len(self.action_space)):
+                action_a_faire = self.Pi[valeur_s]
+                list_succ = mdp[cle_s][action_a_faire]
+                for noeud in list_succ:
+                    somme +=noeud[0]*( noeud[2] + self.gamma*self.V[valeur_s])
+                self.V[valeur_s] = somme
+            for cle_s,valeur_s in statedic.items():
+                if(cle_s not in mdp.keys()): continue
+                li = np.zeros(self.action_space.n)
+                for a in range(self.action_space.n):
                     somme = 0
-                    for j in range(len(state)):
-                        somme += mdp[i][a][j][0]*( mdp[i][a][j][2] + self.gamma*self.V[j])
+                    list_succ = mdp[cle_s][a]
+                    for noeud in list_succ:
+                        entier_noeud = statedic[noeud[1]]
+                        somme +=noeud[0]*( noeud[2] + self.gamma*self.V[entier_noeud])
                     li[a] = somme
-                self.Pi[i] = np.argmax(li)
-
-
-
+                self.Pi[valeur_s] = np.argmax(li)
+            if(np.linalg.norm(ancien_V-self.V) < epsilon): break
 
 
 if __name__ == '__main__':
@@ -49,20 +55,21 @@ if __name__ == '__main__':
 
     env = gym.make("gridworld-v0")
     env.seed(0)  # Initialise le seed du pseudo-random
-    print(env.action_space)  # Quelles sont les actions possibles
-    print(env.step(1))  # faire action 1 et retourne l'observation, le reward, et un done un booleen (jeu fini ou pas)
-    env.render()  # permet de visualiser la grille du jeu 
+    env.render()  # permet de visualiser la grille du jeu
     env.render(mode="human") #visualisation sur la console
+
     statedic, mdp = env.getMDP()  # recupere le mdp : statedic
+    print(statedic.keys())
+    print(mdp.keys())
+
+    print(list(mdp.keys())[0])
+    print(list(mdp.values())[0])
+
     print("Nombre d'etats : ",len(statedic))  # nombre d'etats ,statedic : etat-> numero de l'etat
-    state, transitions = list(mdp.items())[0]
-    print(state)  # un etat du mdp
-    print(transitions)  # dictionnaire des transitions pour l'etat :  {action-> [proba,etat,reward,done]}
-    print("*************************")
-    print(transitions[0])
+
     # Execution avec un Agent
-    agent = PolicyAgent(env.action_space)
-    agent.fit(mdp)
+    agent = PolicyAgent(env.action_space,statedic)
+    agent.fit(statedic,mdp)
 
     # Faire un fichier de log sur plusieurs scenarios
     outdir = 'gridworld-v0/random-agent-results'
@@ -76,7 +83,7 @@ if __name__ == '__main__':
     FPS = 0.0001
     for i in range(episode_count):
         obs = envm.reset()
-        #env.verbose = (i % 100 == 0 and i > 0)  # afficher 1 episode sur 100
+        env.verbose = (i % 100 == 0 and i > 0)  # afficher 1 episode sur 100
         if env.verbose:
             env.render(FPS)
         j = 0
